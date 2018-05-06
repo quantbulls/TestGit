bb1626254329375ba39ffd27bbe8474af155e4ff
Peter Chen <pwchen26@gmail.com>
2018-05-05 22:28:54 -0700

    testing again

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,7 +10,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            System.Console.Out.WriteLine("TestGit");
+            System.Console.Out.WriteLine("TestGit ");
         }
     }
 }
